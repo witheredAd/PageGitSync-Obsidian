da11a5e956b629808998876ee579622d328c4c35
rinz <tiflew123@gmail.com>
2025-12-03 02:34:52 +0800

    chore: add fixme & change manifest & add credit

--- buffer-shim.mjs
+++ buffer-shim.mjs
@@ -1,4 +1,5 @@
 // Thx: https://github.com/Vinzent03/obsidian-git/blob/master/polyfill_buffer.js
+// Thx: https://github.com/isomorphic-git/isomorphic-git/issues/1680
 
 import { Platform } from 'obsidian';
 let buffer;
